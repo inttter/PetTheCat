2608bef517ccfddbeb3a6b85d7f44fef1cb8f49d
Inter <73017070+inttter@users.noreply.github.com>
2023-11-16 20:36:04 +0000

    Update script.js to fetch TheCatAPI
    script.js will now fetch cat images from TheCatAPI, removing the need for having to manually save them into the project folder.
    
    TheCatAPI Documentation: https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const catImages = [
-    'catImages/cat1.png',
-    'catImages/cat2.png',
-    'catImages/cat3.png',
-    'catImages/cat4.png',
-    'catImages/cat5.png',
-    // Add more images with correct relative paths
-  ];
-  
-  const catImage = document.getElementById('catImage');
-  const meowSound = document.getElementById('meowSound');
-  
-  // Function to get a random cat image from the catImages array
-  function getRandomCatImage() {
-    const randomIndex = Math.floor(Math.random() * catImages.length);
-    return catImages[randomIndex];
-  }
-  
-  // Waiting for user to click on the cat image and play the meow sound
-  catImage.addEventListener('click', () => {
-    meowSound.play();
-  });
-  
-  // Sets a random cat image on page load
-  window.addEventListener('load', () => {
-    const randomCatImage = getRandomCatImage();
-    catImage.src = randomCatImage;
-  });
+  'catImages/cat1.png',
+  'catImages/cat2.png',
+  'catImages/cat3.png',
+  'catImages/cat4.png',
+  'catImages/cat5.png',
+  // Add more images with correct relative paths
+];
+
+const catImage = document.getElementById('catImage');
+const meowSound = document.getElementById('meowSound');
+
+function getRandomCatImage() {
+  fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => response.json())
+    .then(data => {
+      const imageUrl = data[0].url;
+      const img = new Image();
+      img.onload = function() {
+        catImage.src = imageUrl;
+      };
+      img.src = imageUrl;
+    })
+    .catch(error => {
+      console.error('Error fetching cat image:', error);
+    });
+}
+
+function setRandomCatImage() {
+  const randomCatImage = getRandomCatImage();
+  catImage.src = randomCatImage;
+}
+
+window.addEventListener('load', setRandomCatImage);
+
+catImage.addEventListener('click', () => {
+  meowSound.play();
+});
